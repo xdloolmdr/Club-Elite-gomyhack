3a11213460ee41d9cb0f9636ae20fff528bfbc88
xdloolmdr <hamoudat1990@gmail.com>
2025-02-19 15:14:01 +0100

    installing daisyui

---
+++ frontend/src/app/home/Home.jsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="bg-[url('/public/images/bgstadium.jpg')] bg-cover bg-center h-screen w-full">
+        <p></p>
+        <button className="btn btn">Go To</button>
+    </div>
+  )
+}
